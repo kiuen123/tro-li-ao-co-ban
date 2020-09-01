38cc70fc4088d5bad4f99c5e9952303984ad03d4
kiuen123 <kien121999121999@gmail.com>
2020-09-01 12:51:27 +0700

    first commit

---
+++ alpha.py
@@ -0,0 +1,44 @@
+#import
+from datetime import date, datetime
+import speech_recognition
+import pyttsx3
+
+#init
+ai_ear = speech_recognition.Recognizer()
+ai_mouth = pyttsx3.init()
+while True:
+    #ear
+    with speech_recognition.Microphone() as mic:
+        print("Alpha: I'm listening")
+        audio = ai_ear.listen(mic)
+    print("Alpha: ... ")
+    try:
+        you = ai_ear.recognize_google(audio)
+    except:
+        you = ""
+        
+    print("You: " + you)
+
+    #brain
+    if you=="hi" or you=="hello":
+        ai_brain = "hi master"
+    elif "today" in you:
+        today = date.today()
+        ai_brain = "today is " + today.strftime("%B %d, %Y")
+    elif "time" in you:
+        now = datetime.now()
+        ai_brain = "it is " + now.strftime("%H hours %M minutes")
+    elif "bye" in you:
+        ai_brain = "good bye master have a good time"
+        print("Alpha : "+ ai_brain)
+        ai_mouth.say(ai_brain)
+        ai_mouth.runAndWait()
+        break
+    else:
+        ai_brain = "i can hear you"
+
+    print("Alpha : "+ ai_brain)
+
+    #mouth
+    ai_mouth.say(ai_brain)
+    ai_mouth.runAndWait()
