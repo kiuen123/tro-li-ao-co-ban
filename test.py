2a34118e6c0cb6dc91b3bd856345f3ff3420ce76
kiuen123 <kien121999121999@gmail.com>
2020-09-10 21:11:43 +0700

    update test

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
-#import touch
-#touch.touch("test")
+# import touch
+# touch.touch("test")
 
-import os
-path = "AI_alpha/test"
-path = os.path.realpath(path)
-os.startfile(path)
+# import os
+# path = "AI_alpha/test"
+# path = os.path.realpath(path)
+# os.startfile(path)
+
