f33aecdec5d94ebd43cc07e9553b4c2c73855697
kiuen123 <kien121999121999@gmail.com>
2020-09-15 11:30:08 +0700

    update weather

--- alpha.py
+++ alpha.py
@@ -33,40 +33,44 @@ while True:
     #brain
     if "hi" in you or "hello" in you:
         ai_brain = "hi master"
+
     elif "today" in you:
         today = date.today()
         ai_brain = "today is " + today.strftime("%B %d, %Y")
+
     elif "time" in you:
         now = datetime.now()
         ai_brain = "it is " + now.strftime("%H hours %M minutes")
+
     elif "open" in you:
         path = "test"
         path = os.path.realpath(path)
         os.startfile(path)
         ai_brain = "Done"
+
     elif "weather" in you:
         response = requests.get("http://api.openweathermap.org/data/2.5/weather?appid=b285f504d19f761f32c51644c825faaa&q=hanoi")
         x = response.json()
-        if x["cod"] != "404":
-            # key in variable y 
-            y = x["main"] 
-            # store the value corresponding 
-            # to the "temp" key of y 
-            current_temperature = y["temp"] - 273
-            # store the value corresponding 
-            # to the "pressure" key of y 
-            current_pressure = y["pressure"] 
-            # store the value corresponding 
-            # to the "humidity" key of y 
-            current_humidiy = y["humidity"] 
-            # store the value of "weather" 
-            # key in variable z 
-            z = x["weather"]
-            # store the value corresponding  
-            # to the "description" key at  
-            # the 0th index of z 
-            weather_description = z[0]["description"] 
-            ai_brain= " Temperature (in kelvin unit) = " + str(current_temperature) + " description = " + str(weather_description)
+        # key in variable y 
+        y = x["main"] 
+        # store the value corresponding 
+        # to the "temp" key of y 
+        current_temperature = y["temp"] - 273
+        # store the value corresponding 
+        # to the "pressure" key of y 
+        current_pressure = y["pressure"] 
+        # store the value corresponding 
+        # to the "humidity" key of y 
+        current_humidiy = y["humidity"] 
+        # store the value of "weather" 
+        # key in variable z 
+        z = x["weather"]
+        # store the value corresponding  
+        # to the "description" key at  
+        # the 0th index of z 
+        weather_description = z[0]["description"] 
+        ai_brain= " Temperature (in kelvin unit) = " + str(current_temperature) + " description = " + str(weather_description)
+        
     elif "bye" in you or "stop" in you:
         ai_brain = "good bye master. I hope you have a good time"
         print("Alpha : "+ ai_brain)
