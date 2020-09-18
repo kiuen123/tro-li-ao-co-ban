33238e04a345257342e3ffab9aac27e863f3614e
kiuen123 <kien121999121999@gmail.com>
2020-09-18 07:51:46 +0700

    update test

--- test.py
+++ test.py
@@ -1,72 +1,57 @@
-# import touch
-# touch.touch("test")
+#import touch
+#touch.touch("test")
 
-# import os
-# path = "AI_alpha/test"
-# path = os.path.realpath(path)
-# os.startfile(path)
+#import os
+#path = "AI_alpha/test"
+#path = os.path.realpath(path)
+#os.startfile(path)
 
 # Python program to find current  
 # weather details of any city 
 # using openweathermap api 
 
-#  
 ## import required modules 
 #import requests, json 
-#  
 ## Enter your API key here 
 #api_key = "b285f504d19f761f32c51644c825faaa"
-#  
 ## base_url variable to store url 
 #base_url = "http://api.openweathermap.org/data/2.5/weather?"
-#  
 ## Give city name 
 #city_name = input("Enter city name : ") 
-#  
 ## complete_url variable to store 
 ## complete url address 
 #complete_url = base_url + "appid=" + api_key + "&q=" + city_name 
-#  
 ## get method of requests module 
 ## return response object 
 #response = requests.get(complete_url) 
-#  
 ## json method of response object  
 ## convert json format data into 
 ## python format data 
-#x = response.json() 
-#  
+#x = response.json()
 ## Now x contains list of nested dictionaries 
 ## Check the value of "cod" key is equal to 
 ## "404", means city is found otherwise, 
 ## city is not found 
 #if x["cod"] != "404": 
-#  
 #    # store the value of "main" 
 #    # key in variable y 
 #    y = x["main"] 
-#  
 #    # store the value corresponding 
 #    # to the "temp" key of y 
 #    current_temperature = y["temp"] 
-#  
 #    # store the value corresponding 
 #    # to the "pressure" key of y 
 #    current_pressure = y["pressure"] 
-#  
 #    # store the value corresponding 
 #    # to the "humidity" key of y 
 #    current_humidiy = y["humidity"] 
-#  
 #    # store the value of "weather" 
 #    # key in variable z 
 #    z = x["weather"] 
-#  
 #    # store the value corresponding  
 #    # to the "description" key at  
 #    # the 0th index of z 
 #    weather_description = z[0]["description"] 
-#  
 #    # print following values 
 #    print(" Temperature (in kelvin unit) = " +
 #                    str(current_temperature) + 
@@ -76,16 +61,14 @@
 #                    str(current_humidiy) +
 #          "\n description = " +
 #                    str(weather_description)) 
-#  
 #else: 
 #    print(" City Not Found ") 
 
-
-from googletrans import Translator
-translator = Translator()
-translator = Translator(service_urls=[
-    'translate.google.com',
-    'translate.google.co.vi',
-])
-translation = translator.translate("hello", dest='vi')
-print(translation.origin, ' -> ', translation.text)
+#from googletrans import Translator
+#translator = Translator()
+#translator = Translator(service_urls=[
+#    'translate.google.com',
+#    'translate.google.co.vi',
+#])
+#translation = translator.translate("hello", dest='vi')
+#print(translation.origin, ' -> ', translation.text)
