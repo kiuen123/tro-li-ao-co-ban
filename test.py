f551e4f79f0e11a65c5623be521f45bab8c2ecc1
kiuen123 <kien121999121999@gmail.com>
2020-09-21 01:53:58 +0700

    update test

--- test.py
+++ test.py
@@ -63,7 +63,6 @@
 #                    str(weather_description)) 
 #else: 
 #    print(" City Not Found ") 
-
 #from googletrans import Translator
 #translator = Translator()
 #translator = Translator(service_urls=[
@@ -72,3 +71,64 @@
 #])
 #translation = translator.translate("hello", dest='vi')
 #print(translation.origin, ' -> ', translation.text)
+
+#from playsound import playsound
+#playsound('MyFolder/Music/Neko Atsume Honkai Impact 3.mp3')
+
+#import musicplayer, sys, os, fnmatch, random, pprint, Tkinter
+#class Song:
+#        def __init__(self, fn):
+#                self.url = fn
+#                self.f = open(fn)
+#        # `__eq__` is used for the peek stream management
+#        def __eq__(self, other):
+#                return self.url == other.url
+#        # this is used by the player as the data interface
+#        def readPacket(self, bufSize):
+#                return self.f.read(bufSize)
+#        def seekRaw(self, offset, whence):
+#                r = self.f.seek(offset, whence)
+#                return self.f.tell()
+#files = []
+#def getFiles(path):
+#        for f in sorted(os.listdir(path), key=lambda k: random.random()):
+#                f = os.path.join(path, f)
+#                if os.path.isdir(f): getFiles(f) # recurse
+#                if len(files) > 1000: break # break if we have enough
+#                if fnmatch.fnmatch(f, '*.mp3'): files.append(f)
+#getFiles(os.path.expanduser("MyFolder/Music"))
+#random.shuffle(files) # shuffle some more
+#i = 0
+#def songs():
+#        global i, files
+#        while True:
+#                yield Song(files[i])
+#                i += 1
+#                if i >= len(files): i = 0
+#def peekSongs(n):
+#        nexti = i + 1
+#        if nexti >= len(files): nexti = 0
+#        return map(Song, (files[nexti:] + files[:nexti])[:n])
+## Create our Music Player.
+#player = musicplayer.createPlayer()
+#player.outSamplerate = 96000 # support high quality :)
+#player.queue = songs()
+#player.peekQueue = peekSongs
+## Setup a simple GUI.
+#window = Tkinter.Tk()
+#window.title("Music Player")
+#songLabel = Tkinter.StringVar()
+#def onSongChange(**kwargs): songLabel.set(pprint.pformat(player.curSongMetadata))
+#def cmdPlayPause(*args): player.playing = not player.playing
+#def cmdNext(*args): player.nextSong()
+#Tkinter.Label(window, textvariable=songLabel).pack()
+#Tkinter.Button(window, text="Play/Pause", command=cmdPlayPause).pack()
+#Tkinter.Button(window, text="Next", command=cmdNext).pack()
+#player.onSongChange = onSongChange
+#player.playing = True # start playing
+#window.mainloop()
+
+#import vlc
+#p = vlc.MediaPlayer("MyFolder/Music/Neko Atsume Honkai Impact 3.mp3")
+#p.play()
+
